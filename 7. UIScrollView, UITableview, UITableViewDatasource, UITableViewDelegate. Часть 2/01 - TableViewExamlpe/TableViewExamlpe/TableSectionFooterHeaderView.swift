c744db9efa0383feaaaa4576e80f77220b08211b
Sychev Aleksandr <asychev@sberhealth.ru>
2022-06-26 15:21:50 +0300

    Add lecture #7 materials

---
+++ 7. UIScrollView, UITableview, UITableViewDatasource, UITableViewDelegate. Часть 2/01 - TableViewExamlpe/TableViewExamlpe/TableSectionFooterHeaderView.swift
@@ -0,0 +1,68 @@
+import UIKit
+
+class TableSectionFooterHeaderView: UITableViewHeaderFooterView {
+
+    // MARK: - Subviews
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        label.font = UIFont.preferredFont(forTextStyle: .footnote)
+        
+        return label
+    }()
+
+    // MARK: - Lifecycle
+    
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        
+        tuneView()
+        addSubviews()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Layout
+    
+    override var intrinsicContentSize: CGSize {
+        CGSize(
+            width: UIView.noIntrinsicMetric,
+            height: 44.0
+        )
+    }
+
+    // MARK: - Public
+
+    func update(title: String) {        
+        titleLabel.text = title
+    }
+    
+    // MARK: - Private
+    
+    private func tuneView() {
+        contentView.backgroundColor = .systemGray6
+    }
+    
+    private func addSubviews() {
+        contentView.addSubview(titleLabel)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            titleLabel.leadingAnchor.constraint(
+                equalTo: contentView.leadingAnchor,
+                constant: 20.0
+            ),
+            titleLabel.trailingAnchor.constraint(
+                equalTo:contentView.layoutMarginsGuide.trailingAnchor
+            ),
+            titleLabel.heightAnchor.constraint(equalToConstant: 30.0),
+            titleLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+        ])
+    }
+}
